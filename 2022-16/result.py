0a8a92351f66f93877800719c6f7d5b2656c11e7
anglisano <87499258+anglisano@users.noreply.github.com>
2022-12-26 18:00:19 +0100

    update

--- 2022-16/result.py
+++ 2022-16/result.py
@@ -1,81 +1,68 @@
-def dfs(graph, start, visited, time, pressure):
-    # Mark the current valve as visited
-    visited.add(start)
+from collections import deque
 
-    # Open the current valve and add its flow rate to the pressure
-    pressure += graph[start]['flow']
-    time += 1
+valves = {}
+tunnels = {}
 
-    # Find the next valve to visit
-    max_flow = -1
-    next_valve = None
-    for neighbor in graph[start]['neighbors']:
-        if neighbor not in visited and graph[neighbor]['flow'] > max_flow:
-            max_flow = graph[neighbor]['flow']
-            next_valve = neighbor
+for line in open('2022-16/input_example.txt'):
+    line = line.strip()
+    valve = line.split()[1]
+    flow = int(line.split(";")[0].split("=")[1])
+    targets = line.split("to ")[1].split(" ", 1)[1].split(", ")
+    valves[valve] = flow
+    tunnels[valve] = targets
 
-    # If we have found a valve to visit, move to it and continue the search
-    if next_valve is not None:
-        time, pressure = dfs(graph, next_valve, visited, time, pressure)
+dists = {}
+nonempty = []
 
-    return time, pressure
+for valve in valves:
+    if valve != "AA" and not valves[valve]:
+        continue
+    
+    if valve != "AA":
+        nonempty.append(valve)
 
-def max_pressure(graph, time_limit):
-    # Create a set to track visited valves
-    visited = set()
+    dists[valve] = {valve: 0, "AA": 0}
+    visited = {valve}
+    
+    queue = deque([(0, valve)])
+    
+    while queue:
+        distance, position = queue.popleft()
+        for neighbor in tunnels[position]:
+            if neighbor in visited:
+                continue
+            visited.add(neighbor)
+            if valves[neighbor]:
+                dists[valve][neighbor] = distance + 1
+            queue.append((distance + 1, neighbor))
 
-    # Start the search at valve AA
-    time, pressure = dfs(graph, 'AA', visited, 0, 0)
+    del dists[valve][valve]
+    if valve != "AA":
+        del dists[valve]["AA"]
 
-    # If we have not reached the time limit, continue the search from the remaining unvisited valves
-    while time < time_limit and visited != set(graph.keys()):
-        for valve in graph.keys():
-            if valve not in visited:
-                time, pressure = dfs(graph, valve, visited, time, pressure)
+indices = {}
 
-    return pressure
+for index, element in enumerate(nonempty):
+    indices[element] = index
 
-# Test the solution
+cache = {}
 
-graph = {
-    'AA': {'flow': 0, 'neighbors': ['DD', 'II', 'BB']},
-    'BB': {'flow': 13, 'neighbors': ['CC', 'AA']},
-    'CC': {'flow': 2, 'neighbors': ['DD', 'BB']},
-    'DD': {'flow': 20, 'neighbors': ['CC', 'AA', 'EE']},
-    'EE': {'flow': 3, 'neighbors': ['FF', 'DD']},
-    'FF': {'flow': 0, 'neighbors': ['EE', 'GG']},
-    'GG': {'flow': 0, 'neighbors': ['FF', 'HH']},
-    'HH': {'flow': 22, 'neighbors': ['GG']},
-    'II': {'flow': 0, 'neighbors': ['AA', 'JJ']},
-    'JJ': {'flow': 21, 'neighbors': ['II']}
-}
+def dfs(time, valve, bitmask):
+    if (time, valve, bitmask) in cache:
+        return cache[(time, valve, bitmask)]
+    
+    maxval = 0
+    for neighbor in dists[valve]:
+        bit = 1 << indices[neighbor]
+        if bitmask & bit:
+            continue
+        remtime = time - dists[valve][neighbor] - 1
+        if remtime <= 0:
+            continue
+        maxval = max(maxval, dfs(remtime, neighbor, bitmask | bit) + valves[neighbor] * remtime)
+        
+    cache[(time, valve, bitmask)] = maxval
+    return maxval
 
-time_limit = 30
-
-print(max_pressure(graph, time_limit))  # Expected output: 54
-
-
-
-import collections as c, itertools, re, functools
-
-r = r'Valve (\w+) .*=(\d*); .* valves? (.*)'
-
-V, F, D = set(), dict(), c.defaultdict(lambda: 1000)
-
-for v, f, us, in re.findall(r, open('input.txt').read()):
-    V.add(v) # store node here
-    if f != '0': F[v] = int(f) # stroe flow here
-    for u in us.split(', ') : D[u, v] = 1
-
-for k, i, j in itertools.product(V, V, V):
-    D[i, j] = min(D[i, j], D[i, k] + D[k, j])
-
-@functools.cache
-def search(t, u = 'AA', vs = frozenset(F), e = False):
-    tt = max([F[v] * (t - D[u, v] - 1) + search(t - D[u, v] - 1, v, vs - {v}, e)
-    for v in vs if D[u, v] < t] + [search(26, vs = vs) if e else 0])
-    return tt
-
-print('Part 1: the most pressure you can release is: ' + str(search(30)))
-print('Part 2: the most pressure you and the elephant can release is: ' + str(search(26, e = True)))
-
+print(dfs(30, "AA", 0))
+a=1
--- 2022-16/result1.py
+++ 2022-16/result1.py
@@ -1,18 +1,150 @@
-
-
+from collections import deque
+# from dataclasses import dataclass
 import re
 
-with(open('2022-16/input.txt')) as f:
-    txt = f.read()
 
-pattern = r'Valve ([A-Z]{2}) .*=(\d*); .* valves? (.*)'
-search_pattern=re.findall(pattern, txt)
-graph=dict()
-for i in search_pattern:
-    graph[i[0]]={'flow':int(i[1]), 'neighbors':i[2].split(', ')}
+def clear_imput(path):
+    with (open(path)) as f:
+        txt = f.read()
 
-time_limit=30
+    pattern = r'Valve ([A-Z]{2}) .*=(\d*); .* valves? (.*)'
+    search_pattern = re.findall(pattern, txt)
+    graph = dict()
+    for i in search_pattern:
+        graph[i[0]] = {'flow': int(i[1]), 'neighbors': i[2].split(', ')}
+    return graph
 
+
+# count how many valves with flow diferent than 0
+def count_valves(state):
+    return sum([1 for i in state if state[i]['flow'] != 0])
+
+# calculate distances between valves that are not 0
+# this will help on the next step of bruteforce
+
+
+def calculate_distances(state):
+    nonempty_valves = []  # i for i in state if state[i]['flow']!=0
+    distances = dict()
+    for valve in state:
+        if state[valve]['flow'] == 0 and valve != 'AA':
+            continue
+        if valve != 'AA':
+            nonempty_valves.append(valve)
+
+        # para partir de distancia 0 en el while
+        distances[valve] = {valve: 0, "AA": 0}
+        visited = {valve}
+        queue = deque([(0, valve)])
+        while queue:
+            distance, position = queue.popleft()
+            for neighbor in state[position]['neighbors']:
+                # if distances.get(neighbor,None) is None:
+                #     distances[neighbor]={}
+                if neighbor in visited:
+                    continue
+                visited.add(neighbor)
+                if state[neighbor]['flow'] != 0:
+                    distances[valve][neighbor] = distance+1
+                queue.append((distance+1, neighbor))
+        # clear fake distances
+        del distances[valve][valve]
+        if valve != "AA":
+            del distances[valve]["AA"]
+    return distances
+
+
+def index_int_valves(state):
+    nonempty_valves = [i for i in state if state[i]['flow'] != 0]  #
+    indexes = dict()
+    for idx, valve in enumerate(nonempty_valves):
+        indexes[valve] = idx
+    return indexes
+
+cache={} # memorizaremos la info aki para no repetir calculos
+def dfs(distances, time, valve, bit_opened_valves, state, indexes):
+    if (time,valve,bit_opened_valves) in cache:
+        return cache[(time,valve,bit_opened_valves)]
+    # no puedo recoger las valves abiertas cerradas pk es lista
+    # para minimizar espacio
+    max_flow = 0
+    # print(format(0, '08b'))
+    # given a actual position, time and valves opened
+    # calculate max flow
+    for neighbor in distances[valve]:
+        '''
+        compare bit_opened_valves with bit
+        100000 & 000001 = 0
+        100000 & 000010 = 0
+        100000 & 000100 = 0
+        100000 & 001000 = 0
+        100000 & 010000 = 0
+        100000 & 100000 = 1
+        # esta parte funciona pero no entiendo 100% pk funciona
+        # el bit_opened_valves es un int, y el bit es un int
+        # al comparar con << el bit estamos cogiendo el bit de la posicion
+        # del bit_opened_valves.
+
+        '''
+        bit=1<<indexes[neighbor]
+        # print(format(bit_opened_valves, '08b'))
+        # print(format(bit, '08b'))
+        if bit_opened_valves & bit:
+            continue
+        remaining_time = time-distances[valve][neighbor]
+        if remaining_time < 0:
+            continue
+        max_flow = max(max_flow,
+                       dfs(distances, remaining_time, neighbor, bit_opened_valves | bit, state,indexes) \
+                        + state[valve]['flow']*remaining_time
+                       )
+    cache[(time,valve,bit_opened_valves)]=max_flow
+    return max_flow
+
+
+def main():
+    path='2022-16/input.txt'
+    # path = '2022-16/input_example.txt'
+    graph = clear_imput(path)
+    number_valves = count_valves(graph)
+    distances = calculate_distances(graph)
+    indexes = index_int_valves(graph)
+    print(indexes)
+    # 0 es no valve opened
+    max_flow = dfs(distances, 30, 'AA', 0, graph,indexes)
+    print(max_flow)
+
+    # part 2
+    b=(1<<number_valves)-1
+    '''
+    para 4 valves
+    se haria
+    1111
+    0110
+    1001
+
+    y despues
+    1111
+    1001
+    0110
+
+    nose si se podria reducir a la mitad el calculo por aki...
+    '''
+    print(format(b, '08b'))
+    print(format(b^0, '08b'))
+    print(format(b^1, '08b'))
+    # el elefante hara el recorrido del resto de valves que no hayamos hecho
+    # nosotros
+    max_flow2=0
+    for i in range(b+1):
+        max_flow2=max(max_flow2,
+            dfs(distances,26,'AA',i,graph,indexes),
+            dfs(distances,26,'AA',b ^ i,graph,indexes)
+        )
+    print(max_flow2)
+
+if __name__ == '__main__':
+    main()
 # decide open actual valve or go to other location and open valve
 # you can open valve only if you have enough time, if its not opened yet
 # and if you have enough pressure/flow
